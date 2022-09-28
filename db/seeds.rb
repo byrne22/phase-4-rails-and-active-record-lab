e5531fc841073552f1f546fec957560c70d92b40
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-09-28 20:26:29 +0300

    commit tree 1cc204bd67b27da751533e1433e711b2be05b2e7

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts
 Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 3)
+puts
